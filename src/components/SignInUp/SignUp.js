7f1d1f1b92a44cb5cb07222600edcb8ccd06c6c7
telio-s <telio.s@hotmail.com>
2022-02-28 20:06:40 +0700

    sign up ui connect database

--- src/components/SignInUp/SignUp.js
+++ src/components/SignInUp/SignUp.js
@@ -1,15 +1,115 @@
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch, useSelector } from "react-redux";
 import { closePopup } from "../features/popUpSlice";
+import { register, reset } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import spinner1 from "./../../img/placeholder/Spinner1.svg";
 
 const SignUp = () => {
+  const [msgErrToast, setMsgErrToast] = useState("");
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+    firstname: "",
+    lastname: "",
+    idcard: "",
+    phone: "",
+    bankName: "",
+    bankId: "",
+  });
+  const {
+    username,
+    email,
+    password,
+    password2,
+    firstname,
+    lastname,
+    idcard,
+    phone,
+    bankName,
+    bankId,
+  } = form;
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.authStore
+  );
   const { status } = useSelector((state) => state.popupSignInOut);
+
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("1");
+    if (password !== password) {
+      toast.error("Password do not match.");
+      setMsgErrToast("Password do not match.");
+    } else if (
+      !username ||
+      !email ||
+      !password ||
+      !password2 ||
+      !firstname ||
+      !lastname ||
+      !idcard ||
+      !phone ||
+      !bankName ||
+      !bankId
+    ) {
+      toast.error("Please complete the information.");
+      // console.log(toast);
+      setMsgErrToast("Please complete the information.");
+    } else {
+      const userData = {
+        u_username: username,
+        u_email: email,
+        u_name: firstname,
+        u_bankactname: bankName,
+        u_password: password,
+        u_bankactid: bankId,
+        u_surname: lastname,
+        u_phonenum: phone,
+        u_idcard: idcard,
+      };
+
+      dispatch(register(userData));
+    }
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+      dispatch(closePopup());
+    }
+
+    dispatch(reset());
+
+    return () => {};
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  if (isLoading) {
+    return <img src={spinner1}></img>;
+  }
 
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="bg-white text-black rounded-xl px-10 py-8 rounded-xl w-[20vw]">
+        <div className="bg-white text-black rounded-xl px-10 py-8 min-w-[320px] rounded-xl w-[20vw]">
           {/* header */}
           <div className="flex justify-between">
             <h1 className="font-['SarabunBold']">Sign Up</h1>
@@ -27,77 +127,112 @@ const SignUp = () => {
 
           {/* content */}
           <div className="grid grid-cols-1 gap-2 place-content-center my-4">
-            <div className="grid grid-cols-1 gap-2 mb-2 mt-4 border-2 rounded-lg p-2">
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="username"
-                type="text"
-                placeholder="Username"
-              />
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="email"
-                type="email"
-                placeholder="Email"
-              />
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="password"
-                type="password"
-                placeholder="Password"
-              />
-            </div>
+            <form onSubmit={onSubmit}>
+              <div className="grid grid-cols-1 gap-2 mb-2 mt-4 border-2 rounded-lg p-2">
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="username"
+                  value={username}
+                  type="text"
+                  placeholder="Username"
+                  onChange={onChange}
+                />
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="email"
+                  value={email}
+                  type="email"
+                  placeholder="Email"
+                  onChange={onChange}
+                />
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="password"
+                  value={password}
+                  type="password"
+                  placeholder="Password"
+                  onChange={onChange}
+                />
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="password2"
+                  value={password2}
+                  type="password"
+                  placeholder="Password"
+                  onChange={onChange}
+                />
+              </div>
+              <div className="grid grid-cols-1 gap-2 mb-2 border-2 rounded-lg p-2">
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="firstname"
+                  value={firstname}
+                  type="text"
+                  placeholder="First name"
+                  onChange={onChange}
+                />
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="lastname"
+                  value={lastname}
+                  type="text"
+                  placeholder="Last name"
+                  onChange={onChange}
+                />
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="idcard"
+                  value={idcard}
+                  type="text"
+                  placeholder="Id card"
+                  onChange={onChange}
+                />
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="phone"
+                  value={phone}
+                  type="text"
+                  placeholder="Phone"
+                  onChange={onChange}
+                />
+              </div>
+              <div className="grid grid-cols-1 gap-2 mb-4 border-2 rounded-lg p-2">
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="bankName"
+                  value={bankName}
+                  type="text"
+                  placeholder="Bank name"
+                  onChange={onChange}
+                />
+                <input
+                  className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
+                  name="bankId"
+                  value={bankId}
+                  type="text"
+                  placeholder="Bank id"
+                  onChange={onChange}
+                />
+              </div>
+              {/* error */}
+              <h1
+                className={`${
+                  (isError && message) || msgErrToast
+                    ? "font-['SarabunBold'] text-red-500 mb-2 justify-self-center"
+                    : "hidden"
+                }`}
+              >
+                {message ? message : msgErrToast}
+              </h1>
 
-            <div className="grid grid-cols-1 gap-2 mb-2 border-2 rounded-lg p-2">
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="firstname"
-                type="text"
-                placeholder="First name"
-              />
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="lastname"
-                type="text"
-                placeholder="Last name"
-              />
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="idcard"
-                type="text"
-                placeholder="Id card"
-              />
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="phoneNum"
-                type="text"
-                placeholder="Phone"
-              />
-            </div>
+              <button
+                className="bg-fuchsia-600 text-white rounded-lg w-full py-1 font-['SarabunBold']"
+                type="submit"
+              >
+                Sign In
+              </button>
+            </form>
 
-            <div className="grid grid-cols-1 gap-2 mb-4 border-2 rounded-lg p-2">
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="bankName"
-                type="text"
-                placeholder="Bank name"
-              />
-              <input
-                className="p-1 outline-0 focus:bg-pink-100  rounded-lg"
-                name="bankId"
-                type="text"
-                placeholder="Bank id"
-              />
-            </div>
-
-            {/* error */}
-            <h1 className="hidden font-['SarabunBold'] text-red-500 mb-2 justify-self-center">
-              wrong
-            </h1>
-
-            <button className="bg-fuchsia-600 text-white rounded-lg py-1 font-['SarabunBold']">
-              Sign In
-            </button>
             <div className="flex font-['SarabunBold']">
               <h1>You have an account </h1>
               <button className="ml-1 text-pink-500">Sign In</button>
--- src/components/app/store.js
+++ src/components/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import popUpSlice from "../features/popUpSlice";
+import authReducer from "../features/auth/authSlice";
 
 export default configureStore({
   reducer: {
     popupSignInOut: popUpSlice,
+    authStore: authReducer,
   },
 });
