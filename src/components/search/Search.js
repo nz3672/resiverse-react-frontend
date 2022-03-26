6f2aeecbcfd14aa07d3d72eb860caa7e9202f613
telio-s <telio.s@hotmail.com>
2022-03-26 14:08:29 +0700

    merge attempt

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -20,15 +20,32 @@ const Search = () => {
   if (!isLoaded) return "Loading...";
 
   return (
-    <div className="absolute top-20 left-20 z-50">
-      <SearchBar
-        setPlaceDetails={setPlaceDetails}
-        setPlaceId={setPlaceId}
-        setShowWidget={setShowWidget}
-        isLoaded={isLoaded}
-      />
-      {showWidget && <Widget placeDetails={placeDetails} />}
-    </div>
+    <>
+      <div className="absolute top-0 left-0 z-50 w-[100vw] h-screen">
+        <Navbar
+          setPlaceDetails={setPlaceDetails}
+          setPlaceId={setPlaceId}
+          setShowWidget={setShowWidget}
+          isLoaded={isLoaded}
+        />
+
+        <div className="mt-4 flex">
+          <div className="ml-5 grid-cols-1 desktop:ml-20 grid grid-cols-2 tablet:grid-cols-2 laptop:grid-cols-3 gap-4 place-content-center">
+            {/* show searching widgets */}
+            {/* {showWidget && <Widget placeDetails={placeDetails} />} */}
+            {showWidget && console.log(placeDetails)}
+
+            {/* show fetching widgets */}
+            {!showWidget &&
+              widgets.map((widget, i) => {
+                // console.log(widget);
+                return <Widget key={i} placeDetails={widget} />;
+              })}
+          </div>
+          <div className="w-0 h-0 desktop:w-96 desktop:h-96 phone:w-0 phone:h-0"></div>
+        </div>
+      </div>
+    </>
   );
 };
 
